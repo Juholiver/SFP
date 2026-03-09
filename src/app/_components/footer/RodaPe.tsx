652b32508320809da1eeabf83f922c536f6a37b9
Juholiver <juholiver3@gmail.com>
2026-03-09 20:14:03 -0300

    adc botao whats e links

--- src/app/_components/footer/RodaPe.tsx
+++ src/app/_components/footer/RodaPe.tsx
@@ -1,12 +1,29 @@
+'use client'
+import React from "react";
 import "./RodaPe.css";
 export default function RodaPe() {
+
+    const whatsappNumber = process.env.NEXT_PUBLIC_WHATSAPP_NUMBER;
+
+  const handleCheckout = () => {
+    // Cria uma mensagem simples
+    const message = encodeURIComponent(`Olá! Gostaria de fazer meu pedido.`);
+    
+    // Abre o WhatsApp
+    window.open(`https://wa.me/${whatsappNumber}?text=${message}`, '_blank');
+  };
+
+
     return (
+
+        
         <footer className="footer">
             <div className="footer-container">
 
                 <div className="footer-section">
                     <h3>São Francisco Personalizados</h3>
-                    <p>O melhor lugar para encontrar o que você precisa com qualidade.</p>
+                    <h4>Endereço</h4>
+                    <p>Rua: Vicente Rodrigues Furtado, N: 361, Jardim Casa Grande, Itapetininga</p>
                 </div>
 
                 <div className="footer-section">
@@ -22,17 +39,17 @@ export default function RodaPe() {
                     <h4>Fale Conosco</h4>
                     <div className="contact-whatsapp">
                         <img src="/whatsapp.jpg" alt="WhatsApp" className="contact-image" />
-                        <a href="/contato" className="contact-button-whatsapp">WhatsApp</a>
+                        <button onClick={handleCheckout} className="contact-button-whatsapp">WhatsApp</button>
                     </div>
 
                     <div className="contact-instagram">
                         <img src="/insta.jpg" alt="Instagram" className="contact-image" />
-                        <a href="https://www.instagram.com/" className="contact-button-instagram">Instagram</a>
+                        <a href="https://www.instagram.com/p/C_lmYXUuH2m/" className="contact-button-instagram">Instagram</a>
                     </div>
 
                     <div className="contact-facebook">
                         <img src="/face.png" alt="Facebook" className="contact-image" />
-                        <a href="https://www.facebook.com/" className="contact-button-facebook">Facebook</a>
+                        <a href="https://www.facebook.com/sfranciscopersonalizados/" className="contact-button-facebook">Facebook</a>
                     </div>
                 </div>
 
--- src/app/_components/produtos/ListaProdutos.tsx
+++ src/app/_components/produtos/ListaProdutos.tsx
@@ -3,7 +3,7 @@ import styles from "./Protudos.module.css";
 
 export default function ListaProdutos() {
   const produtos = [
-    { id: 1, title: "Camiseta Preta", description: "Camiseta branca de algodão personalizada", price: 45.00, img: "/camisetapreta.jpg" },
+    { id: 1, title: "Camiseta Preta", description: "Camiseta branca de algodão personalizada", price: 45.00, img: "/Camisetapreta.jpg" },
     { id: 2, title: "Camiseta Branca", description: "Camiseta preta de algodão personalizada.", price: 50.00, img: "/camisetabranca.jpg" },
     { id: 3, title: "Caneca", description: "Caneca de porcelana personalizada.", price: 40.00, img: "/caneca.jpg" },
     { id: 4, title: "Almofada", description: "Almofada de algodão personalizada.", price: 55.00, img: "/almofada.jpg" },
